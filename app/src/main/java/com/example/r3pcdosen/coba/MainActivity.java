147939865fbeb1ad7dd718dcd261a687b72f9385
latiffatunnisa <45616357+latiffatunnisa@users.noreply.github.com>
2018-12-05 12:31:59 +0700

    commit text view

--- app/src/main/java/com/example/r3pcdosen/coba/MainActivity.java
+++ app/src/main/java/com/example/r3pcdosen/coba/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.r3pcdosen.coba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tes1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
